da1dfb1903482463f3cb4c645969fcf20420a241
Lucas Legrand <legrand.lucas0@gmail.com>
2026-02-22 15:09:57 +0100

    ci: GitHub Actions deploy client to Pages + base path config

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? '/cosmic-td/' : '/',
   resolve: {
     alias: {
       '@ect/shared': path.resolve(__dirname, '../shared/src'),
